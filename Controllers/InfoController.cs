4fc95efe69a91251241c24d526dee4753f43da58
Abu Jubair <64865506+AbuJubair7@users.noreply.github.com>
2023-10-01 16:28:05 +0600

    Iniitial commit

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FormApplication.Models;
+
+namespace FormApplication.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Information info)
+        {
+            return View(info);
+        }
+    }
+}
---
+++ Models/Information.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormApplication.Models
+{
+    public class Information
+    {
+        public string name { set; get; }
+        public string id { set; get; } 
+        public string gender { set; get; }
+        public string profession { set; get; }
+        public string[] hobbies { set; get; }
+
+        public Information() { }
+        public Information(string name, string id, string gender, string profession,
+            string[] hobbies)
+        {
+            this.name = name;
+            this.id = id;
+            this.gender = gender;
+            this.profession = profession;
+            this.hobbies = hobbies;
+        }
+    }
+}
