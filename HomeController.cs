49a6ebc6b82f3613d5456cff765b5711d5a4d0f6
Abu Jubair <64865506+AbuJubair7@users.noreply.github.com>
2023-10-08 15:50:33 +0600

    Validation (8-10-23)

---
+++ HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using validation.Models;
+
+namespace validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LogIn s) 
+        {
+            return View(s);
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(SignUp s)
+        {
+            return View(s);
+        }
+    }
+}
---
+++ SignUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace validation.Models
+{
+
+    public class SignUp
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Name must be between 4 and 50 characters")]
+        [RegularExpression(@"^[A-Za-z\s\.\-]+$", ErrorMessage = "Name can only contain letters, spaces, '.', and '-'")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "User ID is required")]
+        [StringLength(12, MinimumLength = 4, ErrorMessage = "User ID must be between 4 and 12 characters")]
+        [RegularExpression(@"^[A-Za-z0-9_-]+$", ErrorMessage = "User ID can only contain letters, numbers, '-', and '_'")]
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "ID is required")]
+        [RegularExpression(@"^\d{2}-\d{5}-\d$", ErrorMessage = "ID must be in the format 'xx-xxxxx-x' and contain only numbers.")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^\d{2}-\d{5}-\d@student.aiub.edu$", ErrorMessage = "Email must be in the format 'xx-xxxxx-x@student.aiub.edu'")]
+        public string Email { get; set; }
+    }
+}
